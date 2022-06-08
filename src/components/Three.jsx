f5a4341d2a7246106bfe35699c2812ea20c4e1ea
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-08 15:09:51 +0500

    practise

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-// import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline';
 
 const Three = () => {
-  return (
-    <>
-         {/* <Spline scene="https://prod.spline.design/VciXIYe6kFM1-jZn/scene.splinecode" /> */}
-    </>
-  )
+    return (
+        <>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ex iure sit saepe similique officia in rerum excepturi atque laboriosam? Distinctio adipisci neque officiis ab deserunt temporibus, exercitationem aut ratione!
+            {/* <Spline scene="https://prod.spline.design/VciXIYe6kFM1-jZn/scene.splinecode" /> */}
+            {/* <Spline scene="https://prod.spline.design/VciXIYe6kFM1-jZn/scene.splinecode" /> */}
+
+            <Spline scene="https://prod.spline.design/VciXIYe6kFM1-jZn/scene.splinecode" />
+            {/* <iframe style={{width: 600, height: 600}} src='https://my.spline.design/untitled-d261c69b510b6fd6e4ab180dadcb69a2/'/> */}
+            {/* <img src="/images/1.jpg" className='w-100' alt="" /> */}
+
+
+            <div className="vh-100"></div>
+
+            <div className="three">
+                
+            </div>
+
+        </>
+    )
 }
 
 export default Three
