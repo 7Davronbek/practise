f528c6bf7ce7792287d03ef5d3a5f8dc6214ba0e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-23 17:32:14 +0500

    'practise'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import Header from './components/Header'
-// import Language from './components/Language'
-import SwiperSlider from './components/SwiperSlider'
-import './style/main.css'
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <>
-    <Header/> 
-    <div className="vh-100"></div>
-    <SwiperSlider />
-    {/* <Language /> */}
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './style/main.scss'
-import Three from './components/Three';
 
-import '@splidejs/react-splide/css';
-import Slider from './components/Slider';
-
-
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path='/' exact component={App} />
-      <Route path='/three' exact component={Three} />
-      <Route path='/slider' exact component={Slider} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
