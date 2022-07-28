12d198a96f374772a17f73a56d7853bfa0f80211
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-28 11:27:46 +0500

    practise

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './style/main.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
---
+++ src/locales/RU.js
@@ -0,0 +1,3 @@
+export const ru = {
+    logo: 'Логотип'
+}
