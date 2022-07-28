61cd34ffdda0964d0ca616f32111b01d931cf5a0
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-28 12:13:53 +0500

    practise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Language from './componets/Language'
 import Navbar from './componets/Navbar'
 
 const App = () => {
+  const [lang, setLang] = useState(false)
+  const [modal, setModal] = useState(true)
   return (
     <div className='App'>
+      <Language lang={lang} setLang={setLang} setModal={setModal} modal={modal} />
+      {lang ? <>ru</> : <>uz</>}
       <Navbar />
     </div>
   )
