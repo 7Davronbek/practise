634dc9297164cd537afbecd31f7807ee8f81cae7
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-28 10:17:11 +0500

    practise

---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar py-3'>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h2>Logo</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
