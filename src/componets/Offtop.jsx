72bf99d635e71d885eee1d6652c98dcbe945fb09
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-09-07 23:52:07 +0300

    practise makes perfect

--- src/componets/Offtop.jsx
+++ src/componets/Offtop.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Offtop = () => {
+  const [range, setRange] = useState(0);
   return (
-    <div className='Offtop'>
+    <div className="Offtop">
       <div className="container">
         <div className="row">
-            <div className="col-12">
-                <input type="range" name="" id="" />
-            </div>
+          <div className="col-12">
+            <input
+              min={0}
+              max={10}
+              value={range}
+              onChange={(e) => setRange(e.target.value)}
+              type="range"
+              name=""
+              id=""
+            />
+            {range}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Offtop
+export default Offtop;
