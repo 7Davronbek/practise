efbc89a740c816cc9c977568c6da271293b6e98d
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-09-14 22:51:13 +0300

    practise

--- src/componets/Offtop.jsx
+++ src/componets/Offtop.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 const Offtop = () => {
   const [range, setRange] = useState(0);
+
+  const create = async () => {
+    const {data} = await axios.get("http://localhost:8080/user");
+  };
+  useEffect(() => {
+    create()
+  }, [])
   return (
     <div className="Offtop">
       <div className="container">
         <div className="row">
           <div className="col-12">
+            <button>save</button>
             <input
               min={0}
               max={10}
